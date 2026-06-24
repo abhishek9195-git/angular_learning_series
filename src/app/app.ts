ddc9ba8222dd668764a503edf37b2d175ff6f88a
abhishek <abhishek.km2010@gmail.com>
2026-06-24 16:20:51 +0530

    feat: updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+const emailValidatorFn: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const value = control.value as string
+
+  if(!value.includes('@')) {
+    return {invalidEmail: true}
+  }
+  return null
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular_learning_series');
+  
+  loginForm!: FormGroup;
+  private readonly fb = inject(FormBuilder);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: new FormControl('test', [emailValidatorFn]),
+      password: new FormControl('')
+    })
+  }
+  
+  onSubmit() {
+    console.log('==> this.loginForm', this.loginForm.get('email')?.errors);
+  }
+
 }
