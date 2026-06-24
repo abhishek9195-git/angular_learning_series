839fcc5daafe468673fdfe1c939aa6b7c9a0c198
abhishek <abhishek.km2010@gmail.com>
2026-06-24 14:55:38 +0530

    updates - signal form fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
 import { Component, signal } from '@angular/core';
+import { form, FormField, maxLength, minLength, required } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
+
+interface loginFormData {
+  email: string 
+  password: string 
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormField],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular_learning_series');
+
+  loginFormModel = signal<loginFormData>({email: 'abc@xyz.com', password: 'test'})
+
+  loginForm = form(this.loginFormModel, (form) => {
+    required(form.email),
+    minLength(form.email, 15),
+    maxLength(form.email, 20),
+
+    required(form.password),
+    minLength(form.password, 8, {message: 'Password must be of atleast 8 chracters.'})
+  })
+
+  onSubmit(event: Event) {
+    console.log('=====> Email');
+    console.log(this.loginForm.email().errors());
+    console.log(this.loginForm.email().getError('minLength')?.message);
+
+    for(let e of this.loginForm.email().errors()) {
+      console.log(e.kind)
+    }
+
+    console.log('=====> Password');
+    console.log(this.loginForm.password().errors());
+    console.log(this.loginForm.password().getError('minLength')?.message);
+
+
+    event.preventDefault()
+    event.stopPropagation()
+  }
 }
